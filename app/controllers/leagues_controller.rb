ecb90a60f5db1ae40a3c48e4aa21bcf73f1e1249
Tripp Roberts <tripp@tripp-roberts.com>
2016-04-20 17:47:35 -0400

    Alphabetize leagues and competitors

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,10 +1,10 @@
 class LeaguesController < ApplicationController
   def index
-    @leagues = League.all
+    @leagues = League.all.sort_by { |l| l.name }
   end
 
   def competitors
     @league = League.find(params[:id])
-    @competitors = @league.competitors
+    @competitors = @league.competitors.sort_by { |c| c.name}
   end
 end
